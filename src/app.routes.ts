0cb25d1d5bb35dfe2684aa0a04b481874bbd57f1
Jonas Heubuch <jonas.heubuch@outlook.com>
2023-01-15 16:41:49 +0100

    enabled lazy loading for privacy component

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from "@angular/router";
-import { PrivacyComponent } from "./app/privacy/privacy.component";
 import { StartComponent } from "./app/start/start.component";
 
 export const APP_ROUTES: Routes = [
     { path: '', pathMatch: 'full', component: StartComponent },
-    { path: 'privacy', pathMatch: 'full', component: PrivacyComponent }
+    { path: 'privacy', loadComponent: () => import('./app/privacy/privacy.component').then(c => c.PrivacyComponent) }
 ];
