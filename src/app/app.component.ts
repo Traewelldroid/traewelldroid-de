b85e78f2ca7917b3eaf64ff2e188c8245a082dca
Jonas Heubuch <jonas.heubuch@outlook.com>
2025-07-18 15:13:47 +0200

    :arrow_up: Update to Angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet],
-  standalone: true
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [RouterOutlet]
 })
 export default class AppComponent {
 }
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-start',
-  templateUrl: './start.component.html',
-  styleUrls: ['./start.component.scss'],
-  standalone: true,
-  imports: [RouterLink],
+    selector: 'app-start',
+    templateUrl: './start.component.html',
+    styleUrls: ['./start.component.scss'],
+    imports: [RouterLink]
 })
 export default class StartComponent {}
